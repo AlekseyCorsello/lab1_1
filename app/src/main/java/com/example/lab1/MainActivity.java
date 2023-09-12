9d6ea890a307260b2f9a5fda64aa610d21e58853
Aleksey <39133157+AlekseyCorsello@users.noreply.github.com>
2023-09-12 21:22:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.w("DEBUG:", "MainActivity onCreate() called");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btn_to_web = (Button) findViewById(R.id.btn_to_web);
+        btn_to_web.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://developer.android.com/develop/ui/views/layout/linear"));
+                startActivity(intent);
+            }
+        });
+    }
+}
